de20220be371992d507b36ce66f55861b4ad369d
Chris Wall <cbwall24@gmail.com>
2022-07-14 13:00:22 +0300

    added axios and searchCocktail function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({})
+  const [cocktail, setCocktail] = useState('')
+
+  const url = `www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+
+  const searchCocktail = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setCocktail('')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input
+        value={cocktail}
+        onChange={event => setCocktail(event.target.value)}
+        onKeyPress={searchCocktail}
+        placeholder='Enter Cocktail'
+        type="text"
+        />
+      </div>
     </div>
   );
 }
